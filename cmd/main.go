9f0fcc014c848563ce4d4b6577e051aaf35fd84a
Aaron Alpar <aaron.alpar@gmail.com>
2026-02-22 12:38:21 -0800

    Add webhook infrastructure with cert-manager TLS

--- cmd/main.go
+++ cmd/main.go
@@ -131,6 +131,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Transaction")
 		os.Exit(1)
 	}
+	if err := backupv1alpha1.SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "Transaction")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
